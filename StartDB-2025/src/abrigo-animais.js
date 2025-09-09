2e4d4f34687dcc13017948fb3b99e9859f806f73
Erickpatro <erick.patroclos@gmail.com>
2025-09-09 18:31:53 -0300

    Update abrigo-animais.js

--- StartDB-2025/src/abrigo-animais.js
+++ StartDB-2025/src/abrigo-animais.js
@@ -10,39 +10,21 @@ class AbrigoAnimais {
 
     var finalResult = result.length;
 
-   //console.log("String animais --> " + value);
-
-    //console.log("Split animais --> " + result);
-
-    //console.log("get Length animais --> " + finalResult);
-
-   //console.log("get Length2 animais --> " + value.length);
-
-
+  
     if(finalResult > 1 & finalResult <= 2){
 
-      //console.log("Entrei no IF --> ");
-
+     
       var animal1 = result[1];
       var animal2 = result[0];
       
       lista.push(animal1+" - abrigo");
       lista.push(animal2+" - pessoa 1");
 
-      //console.log("Animais value --> "+value);
-
-      //console.log("Animais --> "+animal1, animal2);
-
-      //for(var i =0; i < value; i++){}
+      
 
       var animais = ordemAnimais.split(",");
 
-       //lista.push(brinquedosPessoa1, brinquedosPessoa2,ordemAnimais);
-
-      //console.log("Retorna a minha Lista[0] --> "  +lista[0]);
-      //console.log("Retorna a minha Lista[1] --> "  +lista[1]);
-
-      // '\x1b[34m%s\x1b[0m' = 'Printa em linha VERDE'
+    
       console.log('\x1b[34m%s\x1b[0m', "Call for 'abrigo2' --> "+(lista));
 
       return {
@@ -65,23 +47,21 @@ class AbrigoAnimais {
       lista.push(animal3+" - abrigo");
 
 
-      //console.log("Lista Sem ser ordenada" + lista);
+      
 
       lista.sort();
 
-      //console.log("Lista ordenada "+ lista);
+      
 
       for(var i = 0; i < result.length; i++){
 
-        //console.log("Animal Pair --> "+result[i]);
-
-         //console.log("Total --> "+lista[i]);
+     
 
 
         
       }
 
-      // '\x1b[32m%s\x1b[0m' = 'Printa em linha AZUL'
+      
       console.log('\x1b[32m%s\x1b[0m',"Call for 'abrigo3' --> "+(lista));
 
       return  {
@@ -95,7 +75,7 @@ class AbrigoAnimais {
  
     } else{
 
-      // '\x1b[31m%s\x1b[0m' = 'Printa em linha VERMELHA'
+   
       console.log('\x1b[31m%s\x1b[0m',"Call for 'abrigo1' --> "+"{erro: 'Animal inválido'}");
 
        return {
@@ -103,7 +83,7 @@ class AbrigoAnimais {
       };
     }
 
-    //console.log("Run the intire list result ---> ")+console.log(lista);
+  
 
   }
   
